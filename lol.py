c175ccdf6f80df6008c4a320e7e61206a990a8ec
StianJohnsen <stianjohnzz1@gmail.com>
2022-09-29 22:20:45 +0200

    Har klart å fange opp alle elementer med den valuen

--- lol.py
+++ lol.py
@@ -1,9 +1,3 @@
-def return_key(val):
-    for key, value in start_dict.items():
-        if value==val:
-            return key
-            
-    return('Key Not Found')
 
 lst_name = [
     [],
@@ -14,31 +8,31 @@ lst_name = [
     []
 ]
 
-
-with open('Personer.dta','r',encoding="latin-1") as file:
-    
+with open('Personer.dta', 'r', encoding="latin-1") as file:
     data = file.read()
-    data_la = data.replace('\n',';').split(';')
-    for i in range(len(data_la)):  
-        if i%5 == 0:
+    data_la = data.replace('\n', ';').split(';')
+    for i in range(len(data_la)):
+        if i % 5 == 0:
             lst_name[0].append(data_la[i])
-        elif i%5 == 1:
+        elif i % 5 == 1:
             lst_name[1].append(data_la[i])
-        elif i%5 == 2:
+        elif i % 5 == 2:
             lst_name[2].append(data_la[i])
-        elif i%5 == 3:
+        elif i % 5 == 3:
             lst_name[3].append(data_la[i])
         else:
             lst_name[4].append(data_la[i])
 
-#for i in range(-5,0,1):
-    #print(lst_name[0][i],lst_name[1][i],lst_name[2][-i],lst_name[3][i],lst_name[4][i])
-#print(lst_name[0][-5])
 
 
-#Svar på oppgave 2 print(len(set(lst_name[3])))
+# for i in range(-5,0,1):
+# print(lst_name[0][i],lst_name[1][i],lst_name[2][-i],lst_name[3][i],lst_name[4][i])
+# print(lst_name[0][-5])
 
-#Oppgave 3 
+
+# Svar på oppgave 2 print(len(set(lst_name[3])))
+
+# Oppgave 3
 
 start_dict = {}
 
@@ -46,13 +40,16 @@ for i in lst_name[0]:
     start_dict[i] = 0
 
 for i in lst_name[0]:
-    start_dict[i]+=1
+    start_dict[i] += 1
 
 sorted_values = sorted(start_dict.values())
 set_sorted_values = set(sorted_values)
-print(sorted_values)
-#five_highest_set_values = [x for x in sorted_values[-1:-11:-1]]
-#for i in five_highest_set_values:
+
+
+five_highest_set_values = [x for x in sorted_values[-1:-11:-1]]
+for i in five_highest_set_values:
+    print([x for x,y in start_dict.items() if y == i])
+# for i in five_highest_set_values:
 #    print(i,return_key(i))
 
-#print(start_dict.items())
+# print(start_dict.items())
