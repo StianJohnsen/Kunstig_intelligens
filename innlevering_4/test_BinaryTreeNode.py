acf5ce0f6cd4477566ba2753496e3d04f7f149f5
StianJohnsen <stianjohnzz1@gmail.com>
2022-10-06 21:27:57 +0200

    Har såvidt startet på innlevering nr 4. Har fortsatt mye igjen

--- innlevering_4/test_BinaryTreeNode.py
+++ innlevering_4/test_BinaryTreeNode.py
@@ -1,4 +1,17 @@
 from BinaryTreeNode import BinaryTreeNode
+from collections import namedtuple
 
-test_instance = BinaryTreeNode('5')
-print(test_instance)
+
+Person = namedtuple('Person',['etternavn','fornavn','adresse','postnummer','poststed'])
+
+test_person = Person('KRISTIANSEN','MORTEN KRISTIAN','LEINANYTTA 36','7224','MELHUS')
+
+test_BinaryTree_Node = BinaryTreeNode(test_person)
+
+def test_value_get():
+    assert test_BinaryTree_Node.value.etternavn == 'KRISTIANSEN'
+
+def test_value_set():
+    new_test_BinaryTree_Node = test_BinaryTree_Node.value._replace(etternavn='ELLINGSEN')
+    assert new_test_BinaryTree_Node.etternavn == 'ELLINGSEN'
+
